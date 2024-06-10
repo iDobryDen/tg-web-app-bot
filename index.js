7562fab97f60a13a2072aa7d7232b4eeb9f1f7bb
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-06-10 05:41:50 +0400

    3 buttons work!

---
+++ index.js
@@ -0,0 +1,29 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '5737583166:AAGdOq9XW27hmySWQtARVwvA2BYthdP4qZg';
+const webAppUrl = 'https://ya.ru';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async(msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if(text === '/start'){
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+        reply_markup: {
+            keyboard: [
+                [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
+            ]
+        }
+    })
+
+    await bot.sendMessage(chatId, 'Заходи в наш интернет магазин', {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+            ]
+        }
+    })
+  }
+});
