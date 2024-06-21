16d108a7e5cbd3dd4b7fa15699a7f8b69baea523
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-06-22 02:15:18 +0400

    add page with form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '5737583166:AAGdOq9XW27hmySWQtARVwvA2BYthdP4qZg';
-const webAppUrl = 'https://7adc-78-85-253-114.ngrok-free.app';
+const webAppUrl = 'https://51f7-78-85-253-114.ngrok-free.app';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', async(msg) => {
@@ -13,7 +13,7 @@ bot.on('message', async(msg) => {
     await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
         reply_markup: {
             keyboard: [
-                [{text: 'Заполнить форму', web_app: {url: webAppUrl}}]
+                [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
             ]
         }
     })
